7db5fb8d0c115ab061ea83f72b148e8c73892e27
andrewlau97 <andrewlau1997@hotmail.co.uk>
2025-11-13 18:25:23 +0700

    update project order

--- src/configs/projectHosted.js
+++ src/configs/projectHosted.js
@@ -1,21 +1,9 @@
 const projectHosted = {
-  ncNews: {
-    name: "NC News",
-    weblink: "https://andrew-l-nc-news.netlify.app/",
-    info: "NC News App is a front-end web app that fetches dynamic content from a custom backend. Hosted on Netlify, it used modern libaries and frameworks to deliver a responsive, user-friendly experience.",
-    techStack: ["React", "Vite", "HTML", "CSS", "API", "Material UI"],
-  },
-  tailgotchi: {
-    name: "Tailgotchi",
-    weblink: "https://tailgotchi.netlify.app/",
-    info: "Tailgotchi is an interactive app that makes dog care fun and collaborative. Users can track tasks like feeding, walking, and vet visits, and share pet profiles with family or caretakers to keep everyone in sync.",
-    techStack: ["React", "Supabase", "TypeScript", "Tailwind"],
-  },
-  firstGame: {
-    name: "MonsterJagers",
-    weblink: "https://monsterjagers.netlify.app/",
-    info: "This text-based RPG was inspired by my first coding project on freeCodeCamp. I expanded it to include additional features such as mana, experience leveling, an elemental-based combat system, a more detailed storyline, and visual health bars to represent current and remaining health.",
-    techStack: ["React", "Vite", "HTML", "CSS", "JavaScript", "Supabase"],
+  springwaterWebsite: {
+    name: "Springwater Cantonese",
+    weblink: "https://springwater.netlify.app/",
+    info: "A website for a Chinese restaurant, designed to present the menu, share images of the cuisine, and provide essential contact and location details in a clean, responsive layout.",
+    techStack: ["React", "Vite", "HTML", "CSS", "JavaScript", "Swiper.js"],
   },
   qtEventPlanner: {
     name: "Quest Together",
@@ -23,11 +11,11 @@ const projectHosted = {
     info: "A community-driven event management website that makes it simple to create, organize, and discover events. Designed with a focus on usability and engagement, it helps connect people through shared activities while offering smooth tools for event planning and participation.",
     techStack: ["React", "Vite", "HTML", "CSS", "JavaScript", "Supabase"],
   },
-  springwaterWebsite: {
-    name: "Springwater Cantonese",
-    weblink: "https://springwater.netlify.app/",
-    info: "A website for a Chinese restaurant, designed to present the menu, share images of the cuisine, and provide essential contact and location details in a clean, responsive layout.",
-    techStack: ["React", "Vite", "HTML", "CSS", "JavaScript", "Swiper.js"],
+  tailgotchi: {
+    name: "Tailgotchi",
+    weblink: "https://tailgotchi.netlify.app/",
+    info: "Tailgotchi is an interactive app that makes dog care fun and collaborative. Users can track tasks like feeding, walking, and vet visits, and share pet profiles with family or caretakers to keep everyone in sync.",
+    techStack: ["React", "Supabase", "TypeScript", "Tailwind"],
   },
   weatherApp:{
     name:"WeatherAppAL",
@@ -35,7 +23,18 @@ const projectHosted = {
     info:"A fast and responsive Weather App built with React, Vite, and the Open-Meteo API. It provides real-time and forecast weather information with a modern, minimalist interface.",
     techStack:["React", "Vite", "HTML", "CSS", "JavaScript", "Axios","Open-Meteo API", "Swiper.js"]
   },
-
+  ncNews: {
+    name: "NC News",
+    weblink: "https://andrew-l-nc-news.netlify.app/",
+    info: "NC News App is a front-end web app that fetches dynamic content from a custom backend. Hosted on Netlify, it used modern libaries and frameworks to deliver a responsive, user-friendly experience.",
+    techStack: ["React", "Vite", "HTML", "CSS", "API", "Material UI"],
+  },
+  firstGame: {
+    name: "MonsterJagers",
+    weblink: "https://monsterjagers.netlify.app/",
+    info: "This text-based RPG was inspired by my first coding project on freeCodeCamp. I expanded it to include additional features such as mana, experience leveling, an elemental-based combat system, a more detailed storyline, and visual health bars to represent current and remaining health.",
+    techStack: ["React", "Vite", "HTML", "CSS", "JavaScript", "Supabase"],
+  },
 };
 
 export default projectHosted;
